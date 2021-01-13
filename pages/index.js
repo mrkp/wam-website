c481d6bab9f89e9e2ee1913c21ce1c97c4600b94
mrkp <mark@zed.io>
2021-01-12 21:47:08 -0400

    adding title and favicon to wam website

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+        <Head>
+            <title>WAM!</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Header/>
 
       <main>
