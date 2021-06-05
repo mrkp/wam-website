a2bdbcaa9ddc014ee59043e8beb31886aa956031
Mark P <mark@zed.io>
2021-06-05 09:12:26 -0400

    adding carnival privacy policy

---
+++ pages/carnival-privacy-policy.js
@@ -0,0 +1,76 @@
+
+export default function Home() {
+    return (
+        <div>
+            <main style={{padding: 50}}>
+                <h1>Privacy Policy</h1>
+                <p>
+                    This Privacy Policy describes the policies and procedures of Zed Labs, Inc. (“we,” “our,” or“us”) pertaining to the collection, use, and disclosure of your information on www.wam.money/carnival and related mobile applications and products we offer (the “Services”or “cARnival!”).
+
+                    ‍OVERVIEW
+
+                    Your privacy is important to us. At cARnival!, we follow a few fundamental principles: We don’t ask you for personally identifiable information (defined below). That being said, your contact information, such as your phone number, social media handle, or email address(depending on how you contact us), may be collected when you communicate with us or if you report a bug or other error related to cARnival!. We don’t share your information with third parties except to deliver you our Services and products, comply with the law, makecARnival! better, protect our rights, or effectuate a business transfer. We’re not a huge,faceless corporation. We’re just developers trying to deliver an incredible product. If you have any questions or concerns about this policy, please reach out to us at support@zed.io. HOW YOU ACCEPT THIS POLICY By using cARnival!, including downloading one of our mobile applications, visiting our website, you agree to the use,disclosure, and procedures outlined in this Privacy Policy.
+
+                    ‍WHAT PERSONAL INFORMATION DO WE COLLECT FROM OUR USERS?
+
+                    The information we collect from you falls into two categories: (i) personally identifiable information (i.e., data that could potentially identify you as an individual) (“PersonalInformation”), and (ii) non-personally identifiable information (i.e., information that cannot be used to identify who you are) (“Non-Personal Information”). This Privacy Policy covers both categories and will tell you how we might collect and use each type. We do our best not to collect any Personal Information from cARnival! users. That being said, when using our Services, we do collect PUBlIC wallet addresses that you generate through cARnival!. Further, we may collect some Personal Information from you when you communicate with us, including your contact information, such as your phone number,social media handle, or email address (depending on how you reach out). Like other onlineservices, we also collect a variety of Non-Personal Information, including: Information you create through the cARnival!’s website or mobile applications, including public wallet addresses. Various analytics data, such as: (i) the IP address of the computer you use to access cARnival!; (ii) the type of browser software you are using; (iii) the operating system you are using; (iv) the date and time you access or use cARnival!; (v) thewebsite address, if any, that linked you to cARnival!; (vi) the website address, if any, youleave our website and travel to; and (vii) other non-personally identifiable traffic data.
+
+                    ‍HOW WE COLLECT INFORMATION
+
+                    When You Contact Us. We may collect certain information if you choose to contact us, ifyou use our Services or if you report a bug or other error with cARnival!. This mayinclude contact information such as your name, email address, phone number, and publicwallet address. We, or companies that provide services on our behalf, may also collectcertain Non-Personal Information from you, such as your locally hosted public wallet (a“Wallet”) addresses.
+
+                    ‍Information We Automatically Collect
+
+                    Users who visit our website or use our application may have their device’s IP addresslogged for the purpose of generating anonymous statistics or troubleshooting theperformance of our web servers. Your IP address will not be used to track or identify you,but may be used to determine your geographic location in order to determine which of ourservices you are presented with. Users of our website or mobile applications will receivean anonymous unique device id (“UDID”) for the purpose of identifying the device to cARnival! servers. This UDID will not be tied to users’ identities, but will be used for debuggingpurposes and to differentiate devices when users access our Services using multipledevices.
+
+                    ‍Third Party Services
+
+                    Certain features on cARnival! rely on various third-party products and services(collectively “Third Party Services”), such as the Ethereum network, Google Analytics,Apple’s application platform, Coinbase, Fabric, and Shapeshift. These services may collect certain Personal Information, such as your public Wallet addresses. cARnival! uses Google Analytics, a web analytics service provided by Google, Inc. (“Google”). Google uses cookies to help the website analyze how users use our website. The information generated by the cookie about your use of our website (including your IP address) will be transmitted to and stored by Google on servers in the United States. Google will use this information for the purpose of evaluating your use of the website, compiling reports on website activity for website operators and providing other services relating to website activity and internet usage. Google may also transfer this information to third parties where required to do so by law, or where such third parties process the information on Google’s behalf. Google will not associate your IP address with any other data held by Google. Youmay choose to accept the cookies by selecting the appropriate settings on your browser ifyou do this you may not be able to use the full functionality of our website. By using ourwebsite, you consent to the processing of data about you by Google in the manner and forthe purposes set out above. Please note that your use of these Third Party Services isgoverned by their respective Terms of Service and Privacy Policies. We use and discloseany collected information in accordance with our own Privacy Policy.
+
+                    ‍HOW WE USE THE INFORMATION WE GATHER
+
+                    We primarily use the limited information we collect to enhance cARnival!. Except if wesell all or a portion of our business, or as otherwise described below, we do not rent, trade,or sell your Personal Information. Use of Information to Provide cARnival! to You Some ways we may use your Personal Information are to: Contact you when necessary;Respond to your comments, questions, or issues related to bugs or errors with cARnival!; Provide you with additional information; Send you information and marketing materials about services and products available through cARnival!, using push notifications or other means; Train our team members; or Other internal business purposes. Aggregated Personal Data and Non-Personal Information We may share or disclose aggregated Personal Data or Non-Personal Information with service providers or with other persons we conduct business with, including but not limited potential third-parties for the purpose of showcasing the performance of the company. These service providers and other persons may also share with us aggregated Non-Personal Information that they have independently developed or acquired. Additionally, we may combine aggregate information from the pixel tags, web beacons, and cookies with similar data we collect from other visitors to help us improve our Services. When doing so, we do our best to ensure that the any aggregated information cannot be linked back to you.
+
+                    ‍Agents or Third Party Partners
+
+                    We may provide your Personal Information to our employees, contractors, agents, service providers, and designees (“Agents”) to enable them to perform certain services for us exclusively, including: Improvement of website-related services and features; and Perform maintenance services. Business Transfers We may choose to buy or sell assets. In these types of transactions, customer information is typically one of the business assets that would be transferred. Also, if we (or our assets) are acquired, or if we go out of business,enter bankruptcy, or go through some other change of control, your Personal Information could be one of the assets transferred to or acquired by a third party. By accepting this Privacy Policy, as outlined above, you consent to any such transfer.
+
+                    ‍Protection of Us and Others
+                    ‍
+                    We reserve the right to access, read, preserve, and disclose any information that we reasonably believe is necessary to: comply with the law or a court order; cooperate with law enforcement; enforce or apply our Terms of Use and other agreements; or protect the. rights, property, or safety of cARnival!, our employees, our users, or others.
+                    ‍
+                    ‍WHAT PERSONAL INFORMATION CAN I ACCESS OR CHANGE?
+
+                    You can request access to the information we have collected from you. You can do this by contacting us at support@zed.io. We will make sure to provide you with a copy of the data we process about you. To comply with your request, we may ask you to verify your identity.We will fulfill your request by sending your copy electronically. For any subsequent access request, we may charge you with an administrative fee. If you believe that the informationwe have collected is incorrect, you are welcome to contact us so we can update it andkeep your data accurate. Any data that is no longer needed for purposes specified in the“How We Use the Information We Gather” section will be deleted after ninety (90) days.Wallet addresses created through the cARnival! application cannot be deleted from theEthereum blockchain, therefore we are unable to delete this personal information. If at anypoint you wish for cARnival! to delete information about you, you may contact us at support@zed.io.
+
+                    ‍DATA RETENTION
+
+                    If you delete your Wallet or addresses from the cARnival! Android mobile application,uninstall cARnival! mobile applications from your device, or request that your informationbe deleted, we still may retain some information that you have provided to us to maintaincARnival! or to comply with relevant laws.
+                    ‍
+                    ‍DATA SECURITY
+
+                    We are committed to making sure your information is protected and have selected third-party vendors which use the Ethereum network, including Coinbase, Shapeshift, andChangelly, that help keep your Personal Information safe. Unfortunately, we do not controlthese third parties and therefore cannot guarantee complete security. We employ severalphysical and electronic safeguards to keep your information safe, including encrypted userpasswords, two factor verification and authentication on passwords where possible, and securing all connections with industry standard transport layer security. Even with all theseprecautions, we cannot fully guarantee against the access, disclosure, alteration, ordeletion of data through events, including but not limited to hardware or software failure orunauthorized use. Any information that you provide to us is done so entirely at your ownrisk.
+                    ‍
+                    ‍CHILDREN
+
+                    We are especially sensitive about children’s information. Our Services are not targetedtowards children, and we don’t knowingly collect information from children under the age of13. If you are a parent or legal guardian of a minor child, we will treat any information thatyou provide us while using cARnival! on behalf of your minor child as PersonalInformation as otherwise provided in this Privacy Policy. If you have questions concerningour information practices with respect to children, or if you learn that a child under the ageof 13 has used cARnival!, created a user account, or provided us with personalinformation, please email us at support@zed.io.
+                    ‍
+                    ‍ONLINE TRACKING AND HOW WE RESPOND TO DO NOT TRACK SIGNALS
+
+                    Online tracking is the collection of data about an individual’s Internet activity used todeliver targeted advertisements and for other purposes. Some web browsers (includingSafari, Internet Explorer, Firefox, and Chrome) incorporate a “Do Not Track” (DNT) orsimilar feature that signals to websites that a visitor does not want to have his/her onlineactivity and behavior tracked. If an online service elects to respond to a particular DNTsignal, the service may refrain from collecting certain personal information about thebrowser’s user. Not all browsers offer a DNT option and there is currently no industryconsensus as to what constitutes a DNT signal. For these reasons, many websiteoperators, including cARnival!, do not take action to respond to DNT signals. For moreinformation about DNT signals, visit http://allaboutdnt.com.
+
+                    ‍CHANGES AND UPDATES TO PRIVACY POLICY
+
+                    We reserve the right to update and revise this privacy policy at any time. We occasionallyreview this Privacy Policy to make sure it complies with applicable laws and conforms tochanges in our business. We may need to update this Privacy Policy, and we reserve theright to do so at any time. If we do revise this Privacy Policy, we will update the “EffectiveDate” at the bottom of this page so that you can tell if it has changed since your last visitand will do our best to notify you. Please review this Privacy Policy regularly to ensure that you are aware of its terms. Any use of cARnival! after an amendment to our PrivacyPolicy constitutes your acceptance to the revised or amended agreement.
+                    ‍
+                    ‍INTERNATIONAL USERS AND VISITORS
+
+                    cARnival! is hosted in the United States. If you are a user accessing the Services fromthe European Union, Asia, or any other region with laws or regulations governing personaldata collection, use, and disclosure that differ from United States laws, please be advisedthat through your continued use of the Services, which is governed by US law, you aretransferring your Personal Information to the United States and you consent to thattransfer.
+                    ‍
+                    ‍QUESTIONS
+
+                    We’d be happy to answer them. Shoot us an email or send us a note:Email: support@zed.io Thanks for reading our Privacy Policy!</p>
+            </main>
+        </div>
+    )
+}
